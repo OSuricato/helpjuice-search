c63055b7df9cf6a25a115342071fb2c049e3c1a6
victor barbosa <barbosavictor30@gmail.com>
2024-01-10 23:48:31 -0300

    controllres created for authors and articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  get 'articles/index'
+  get 'articles/show'
+  get 'authors/index'
+  get 'authors/show'
+  root to: "articles#index"
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :authors, only: [:index, :show] do
     resources :articles, only: [:index, :show]
   end
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
