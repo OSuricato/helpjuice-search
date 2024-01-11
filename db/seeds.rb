db583598a7ebed9d504159e7294b815e38c3d4a1
victor barbosa <barbosavictor30@gmail.com>
2024-01-10 23:29:02 -0300

    database seeded using Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# Create 20 authors
+20.times do
+  author = Author.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+    )
+    puts "Created author #{author.first_name} #{author.last_name}"
+
+  # Create 5 articles for each author
+  5.times do
+    article = Article.create(
+      title: Faker::Book.title,
+      year: Faker::Number.between(from: 1900, to: 2022),
+      content: Faker::Lorem.paragraph(sentence_count: 10),
+      author: author
+      )
+      puts "Created article #{article.title} by #{author.first_name} #{author.last_name}"
+  end
+end
