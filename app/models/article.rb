ca0cdc2b8a530a375295f5e55ce921541b3e25e9
victor barbosa <barbosavictor30@gmail.com>
2024-01-12 11:03:03 -0300

    implemented search function

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,21 @@
 class Article < ApplicationRecord
+  include PgSearch::Model
+
   belongs_to :author
+
+  pg_search_scope :search_by_all,
+    against: {
+      title: 'A',
+      content: 'B',
+      year: 'C'
+    },
+    associated_against: {
+      author: {
+        first_name: 'A',
+        last_name: 'B'
+      }
+    },
+    using: {
+      tsearch: { prefix: true, any_word: true }
+    }
 end
